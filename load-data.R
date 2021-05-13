88414eae8ef9df69803c8910e5130a96d76e770d
John Goodwin <jogoodwin@geico.com>
2021-05-13 18:01:32 -0400

    Circuit name fixes and driver names now include codes

--- load-data.R
+++ load-data.R
@@ -2,15 +2,27 @@
 
 lap_times <- read_csv("./data/lap_times.csv")
 races <- read_csv("./data/races.csv")
+circuits <- read_csv("./data/circuits.csv")
 drivers <- read_csv("./data/drivers.csv")
 results <- read_csv("./data/results.csv")
 
-races <- races %>%
-  select(raceId, year, name) %>%
+circuits <- select(circuits, circuitId, name) %>%
+  rename(circuit_name = name) %>%
+  mutate(circuit_name = str_replace(circuit_name, "Grand Prix ", "")) %>%
+  mutate(circuit_name = case_when(circuit_name == 'Istanbul Park' ~ 'Intercity Istanbul Park',
+                                  circuit_name == 'Circuit Gilles Villeneuve' ~ 'Circuit Gilles-Villeneuve',
+                                  circuit_name == 'Autodromo Nazionale di Monza' ~ 'Autodromo Nazionale Monza',
+                                  circuit_name == 'Suzuka Circuit' ~ 'Suzuka International Racing Course',
+                                  circuit_name == 'A1-Ring' ~ 'Red Bull Ring',
+                                  TRUE ~ circuit_name))
+
+races <- races %>% 
+  left_join(circuits, by = "circuitId") %>%
+  select(raceId, year, name, circuit_name) %>%
   rename(race_name = name)
 
 drivers <- drivers %>%
-  mutate(driver_name = paste(forename, surname)) %>%
+  mutate(driver_name = paste(forename, surname, paste0("(", code, ")"))) %>%
   select(driverId, driver_name, code) %>%
   rename(name_code = code)
 
@@ -26,7 +38,7 @@ lap_times <- lap_times %>%
   left_join(drivers, by = "driverId") %>%
   left_join(results, by = c("raceId", "driverId")) %>%
   rename(lap_position = position) %>%
-  select(year, race_name, driver_name, name_code, lap, lap_time, lap_position, final_position)
+  select(year, race_name, circuit_name, driver_name, name_code, lap, lap_time, lap_position, final_position)
 
 
 ## Load GeoJSON Data ----
