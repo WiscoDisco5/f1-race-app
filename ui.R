0d536266b662225ac4bfa297e7f02951a4926313
John Goodwin <jgoodwinwi@gmail.com>
2021-05-22 00:49:17 -0400

    Adding a how to description to Docs tab.

--- ui.R
+++ ui.R
@@ -56,12 +56,20 @@ ui <- navbarPage(
       width = 10
     )
   ),
-  tabPanel("About",
+  tabPanel("Documentation",
            mainPanel(
              h4("Purpose"),
-             "This application provides visualizations and maps for Formula 1 races from 2010 to present. Formula 1 is an international auto racing championship involving (as of 2021) 20 drivers, 10 contructors, and races all over the world. More information about how to interpret figures from this application can be found", 
+             "This application provides visualizations and maps for Formula 1 races from 2010 to present. Formula 1 is an international auto racing championship involving (as of 2021) 20 drivers, 10 constructors, and races all over the world. More information about how to interpret figures from this application can be found", 
              a("here.", 
                href = "https://github.com/WiscoDisco5/f1-race-app#readme"),
+             h4("How to Use"),
+             "Visualizations can be selected by:",
+             tags$ol(
+               tags$li("Selecting a race 'Year'"),
+               tags$li("Selecting a 'Race' event name"),
+               tags$li("Selecting a set of 'Drivers'")
+             ),
+             "Since the list of eligible races can vary by year and the list eligible drivers can vary by race and year, you may notice some of the options changing based on your selections. Every time a year is changed, the figures will reset to the first race of the year and the top 3 drivers from the first race. When a new race is selected within a particular year, the driver figures will reset to the top 3 drivers of that race.",
              h4("Sources"),
              "Historical race data comes thanks to",
              a("Rohan Rao's Kaggle project", 
